650c298d281839ec556d85987a8f17c51a7cb2c8
Blugray <162154088+BlugrayGuyLegit@users.noreply.github.com>
2024-06-11 15:23:00 +0200

    Update ytplay.py

--- ytplay.py
+++ ytplay.py
@@ -2,13 +2,13 @@ import discord
 from discord.ext import commands
 import youtube_dl
 
-bot = commands.Bot(command_prefix='g!')
+bot = commands.Bot(command_prefix='g!', intents=discord.Intents.voice_states())
 
 @bot.command(name='play')
 async def play(ctx, url: str):
     voice_channel = ctx.author.voice.channel
     if voice_channel is None:
-        await ctx.send("You must first join a voice room.")
+        await ctx.send("You must first join a voice channel.")
         return
     await voice_channel.connect()
     voice_client = ctx.guild.voice_client
@@ -26,6 +26,7 @@ async def play(ctx, url: str):
         info = ydl.extract_info(url, download=False)
         url2 = info['formats'][0]['url']
         voice_client.play(discord.FFmpegPCMAudio(url2), after=lambda e: print('done', e))
+        await ctx.send("Playing music.")
 
 @bot.command(name='stop')
 async def stop(ctx):
@@ -34,5 +35,6 @@ async def stop(ctx):
         await ctx.send("I am not connected to a voice channel.")
         return
     await voice_client.disconnect()
+    await ctx.send("Stopped playing music.")
 
 bot.run(TOKEN)
